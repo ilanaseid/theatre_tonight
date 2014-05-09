c8cc07015395b82e3135ea59b8b2badc180ccc79
originalblend337 <originalblend337@gmail.com>
2014-05-09 18:39:53 -0400

    added facebook login functioning

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,6 +1,6 @@
 class OmniauthCallbacksController < ApplicationController
 
-	def facebook
+	def all
 		user = User.from_omniauth(request.env["omniauth.auth"])
 		if user.persisted?
 			sign_in_and_redirect_user, flash[:notice] = "Signed in!"
