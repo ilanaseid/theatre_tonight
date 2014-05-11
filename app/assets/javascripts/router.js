3109fc0cbb74218cb42e7554e13e06944bbf21db
originalblend337 <originalblend337@gmail.com>
2014-05-11 14:45:13 -0400

    info expect theatre stuff showing on index

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -19,10 +19,13 @@ var AppRouter = Backbone.Router.extend({
 		this.theatreCollection.fetch({wait: true});
 		this.showCollection.fetch({
 			success: function() {
+				
 				$.each(this.showCollection.models, function(index, value) {
 					value.theatre = theatresReference.findWhere({id: value.attributes.theatre_id});
 				});
+				
 				$('#shows').html(this.showCollectionView.el);
+
 			}.bind(this)
 		});
 	}
--- app/assets/javascripts/views/showCollectionView.js
+++ app/assets/javascripts/views/showCollectionView.js
@@ -2,14 +2,13 @@ var ShowCollectionView = Backbone.View.extend({
 
 	className: 'shows-wrapper',
 	initialize: function() {
-		this.addAll();
+		this.listenTo(this.collection, 'add', this.addOne);
 	},
 	addAll: function() {
 		this.$el.empty();
 		this.collection.each(this.addOne, this);
 	},
 	addOne: function(showModel) {
-
 		var showItemView = new ShowItemView({model: showModel});
 		showItemView.$el.appendTo(this.$el);
 	}
