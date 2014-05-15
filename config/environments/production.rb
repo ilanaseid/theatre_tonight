2669abb3aad6962ff48b7776e1f03c4bbdc61cc0
originalblend337 <originalblend337@gmail.com>
2014-05-15 19:46:46 -0400

    added gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,14 +83,14 @@ Rails.application.configure do
   # config.action_mailer.default_url_options = { host: 'http://desolate-earth-2268.herokuapp.com/' }
 
   # config.action_mailer.perform_deliveries    = true
+  # config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  # # config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
     :address => 'smtp.gmail.com', 
     :port => 587,
     :authentication => :plain,
-    :user_name => ENV['GMAIL_USERNAME'],
-    :password => ENV['GMAIL_PASSWORD'],
+    :user_name => ENV['MAIL_USERNAME'],
+    :password => ENV['MAIL_PASSWORD'],
     :domain => 'desolate-earth-2268.herokuapp.com',
     :enable_starttls_auto => true }
   # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
