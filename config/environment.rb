e46fe85b36709dd427458a758d9a759cf693a9c4
originalblend337 <originalblend337@gmail.com>
2014-05-15 21:37:51 -0400

    switching mail settings to production

--- config/environment.rb
+++ config/environment.rb
@@ -1,18 +1,18 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.perform_deliveries = true
-ActionMailer::Base.raise_delivery_errors = true
-ActionMailer::Base.smtp_settings = {
-  :enable_starttls_auto => true,
-  :address => 'smtp.gmail.com',
-  :port => 587,
-  :domain => 'desolate-earth-2268.herokuapp.com',
-  :authentication => :plain,
-  :user_name => ENV['MAIL_USERNAME'],
-  :password => ENV['MAIL_PASSWORD'],
-}
+# ActionMailer::Base.delivery_method = :smtp
+# ActionMailer::Base.perform_deliveries = true
+# ActionMailer::Base.raise_delivery_errors = true
+# ActionMailer::Base.smtp_settings = {
+#   :enable_starttls_auto => true,
+#   :address => 'smtp.gmail.com',
+#   :port => 587,
+#   :domain => 'desolate-earth-2268.herokuapp.com',
+#   :authentication => :plain,
+#   :user_name => ENV['MAIL_USERNAME'],
+#   :password => ENV['MAIL_PASSWORD'],
+# }
 
 # Initialize the Rails application.
 Rails.application.initialize!
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,17 +82,17 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   # config.action_mailer.default_url_options = { host: 'http://desolate-earth-2268.herokuapp.com/' }
 
-  # config.action_mailer.perform_deliveries    = true
-  # config.action_mailer.raise_delivery_errors = true
-  # config.action_mailer.default_url_options = { host: 'desolate-earth-2268.herokuapp.com' }
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = {
-  #   :address => 'smtp.gmail.com', 
-  #   :port => 587,
-  #   :authentication => :plain,
-  #   :user_name => ENV['MAIL_USERNAME'],
-  #   :password => ENV['MAIL_PASSWORD'],
-  #   :domain => 'desolate-earth-2268.herokuapp.com',
-  #   :enable_starttls_auto => true
-  # }
+  config.action_mailer.perform_deliveries    = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: 'desolate-earth-2268.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => 'smtp.gmail.com', 
+    :port => 587,
+    :authentication => :plain,
+    :user_name => ENV['MAIL_USERNAME'],
+    :password => ENV['MAIL_PASSWORD'],
+    :domain => 'desolate-earth-2268.herokuapp.com',
+    :enable_starttls_auto => true
+  }
 end
