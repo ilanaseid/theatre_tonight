97a7a24147626886e2c9c4bbe2b981997d5ed9a8
originalblend337 <originalblend337@gmail.com>
2014-05-15 20:16:10 -0400

    changed usermailer

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,7 +1,5 @@
 class ChargesController < ApplicationController
 
-# skip_before_filter :verify_authenticity_token
-
 def new
 end
 
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,7 +5,7 @@ class UserMailer < ActionMailer::Base
 	def receipt_email(user)
 		@user = user
 		@url = 'http://desolate-earth-2268.herokuapp.com/'
-		mail(to: user.email, subject: "Thank you for your TheatreTonight purchase!")
+		mail(to: @user.email, subject: "Thank you for your TheatreTonight purchase!")
 	end
 
 end
