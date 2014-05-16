2b175dae036e93c390985e00d86ac5b09e6cf4b4
originalblend337 <originalblend337@gmail.com>
2014-05-15 20:53:47 -0400

    changed enviroinmentrb

--- config/environment.rb
+++ config/environment.rb
@@ -1,15 +1,19 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+# config.action_mailer.perform_deliveries    = true
+# config.action_mailer.raise_delivery_errors = true
+# config.action_mailer.default_url_options = { host: 'desolate-earth-2268.herokuapp.com' }
+# config.action_mailer.delivery_method = :smtp
+# config.action_mailer.smtp_settings = {
+# 	:address => 'smtp.gmail.com', 
+# 	:port => 587,
+# 	:authentication => :plain,
+# 	:user_name => ENV['MAIL_USERNAME'],
+# 	:password => ENV['MAIL_PASSWORD'],
+# 	:domain => 'desolate-earth-2268.herokuapp.com',
+# 	:enable_starttls_auto => true
+# }
+
 # Initialize the Rails application.
 Rails.application.initialize!
-
-ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.sendgrid.net',
-  :port           => '587',
-  :authentication => :plain,
-  :user_name      => ENV['SENDGRID_USERNAME'],
-  :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => 'heroku.com',
-  :enable_starttls_auto => true
-}
