1b8c16eb936a8fc5db5aca43e2fd999580dbba94
originalblend337 <originalblend337@gmail.com>
2014-05-15 20:57:02 -0400

    envrb changed

--- config/environment.rb
+++ config/environment.rb
@@ -1,19 +1,18 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-# config.action_mailer.perform_deliveries    = true
-# config.action_mailer.raise_delivery_errors = true
-# config.action_mailer.default_url_options = { host: 'desolate-earth-2268.herokuapp.com' }
-# config.action_mailer.delivery_method = :smtp
-# config.action_mailer.smtp_settings = {
-# 	:address => 'smtp.gmail.com', 
-# 	:port => 587,
-# 	:authentication => :plain,
-# 	:user_name => ENV['MAIL_USERNAME'],
-# 	:password => ENV['MAIL_PASSWORD'],
-# 	:domain => 'desolate-earth-2268.herokuapp.com',
-# 	:enable_starttls_auto => true
-# }
-
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.perform_deliveries = true
+ActionMailer::Base.raise_delivery_errors = true
+ActionMailer::Base.smtp_settings = {
+  :enable_starttls_auto => true,
+  :address => 'smtp.gmail.com',
+  :port => 587,
+  :domain => 'gmail.com',
+  :authentication => :plain,
+  :user_name => ENV['MAIL_USERNAME'],
+  :password => ENV['MAIL_PASSWORD'],
+}
