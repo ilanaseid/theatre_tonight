c0d50fe7c3df2e772ccef70cd5c83abb2638bfa5
originalblend337 <originalblend337@gmail.com>
2014-05-28 10:24:52 -0400

    made port number a string

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address => 'smtp.gmail.com', 
-    :port => 587,
+    :port => '587',
     :authentication => 'plain',
     :user_name => ENV['MAIL_USERNAME'],
     :password => ENV['MAIL_PASSWORD'],
